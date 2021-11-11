1f5a9bad7de1af82e020a7ac0838e2e90de0420a
Kira2837 <62303970+Kira2837@users.noreply.github.com>
2021-11-11 16:09:03 +0300

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#define LENGTH 100
+
+int main()
+{
+    size_t i, len;
+    char str[LENGTH+1];
+
+    scanf("%[^\n]s", str);
+
+    len = strlen(str);
+
+    for(i = 0;i < len;i++)
+    {
+        if(('a' <= str[i]) && (str[i] <= 'z'))
+        {
+            str[i] = str[i]-32;
+        }
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
