4bdb1c6108c437cbc59478316c48e719479276e4
Kira2837 <62303970+Kira2837@users.noreply.github.com>
2022-01-12 16:43:40 +0300

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main ()
+{
+    char big_str[20];
+    char sml_str[10];
+
+    scanf("%49[^\n]s", big_str);
+
+    strncpy(sml_str, big_str, sizeof(sml_str)-1);
+
+    printf("%s", sml_str);
+}
